dfdf88f80db3a1577a6fa055286801722d201fbf
Aga Rudnicka <ant6ant@gmail.com>
2017-01-01 23:37:30 +0100

    Improved gesture recognition

--- main.py
+++ main.py
@@ -1,23 +1,27 @@
+import math
 import tkFont
 import numpy as np
 import cv2
-from subprocess import check_call
 import Tkinter as tk
 import thread
 import Queue
-import sys
 
-import Camera
 from commands import *
 
 request_queue = Queue.Queue()
 result_queue = Queue.Queue()
 t = None
+debug = False
 
 
-def submit_to_tkinter(callable, *args, **kwargs):
-    request_queue.put((callable, args, kwargs))
+def submit_to_tkinter(cb, *args, **kwargs):
+    request_queue.put((cb, args, kwargs))
     return result_queue.get()
+
+
+def debug_toggle():
+    global debug
+    debug = not debug
 
 
 def main_tk_thread():
@@ -25,28 +29,27 @@ def main_tk_thread():
 
     def timertick():
         try:
-            callable, args, kwargs = request_queue.get_nowait()
+            cb, args, kwargs = request_queue.get_nowait()
         except Queue.Empty:
             pass
         else:
-            # print "something in queue"
-            retval = callable(*args, **kwargs)
+            retval = cb(*args, **kwargs)
             result_queue.put(retval)
-
         t.after(10, timertick)
 
     t = tk.Tk()
-    text = tk.Text()
     font = tkFont.Font(family="Arial", size=18, weight=tkFont.BOLD)
     t.configure(width=320, height=320)
-    # b = tk.Button(text='test', name='button', command=tk._exit)
-    # b.place(x=0, y=0)
+    b = tk.Button(text='debug', name='button', command=debug_toggle)
+    b.place(x=20, y=200)
     hull = tk.Label(t, name="hull", text="None", font=font)
     hull.place(x=20, y=10)
     defects = tk.Label(t, name="defects", text="None", font=font)
     defects.place(x=20, y=60)
+    defects_filtered = tk.Label(t, name="defects_filtered", text="None", font=font)
+    defects_filtered.place(x=20, y=110)
     command = tk.Label(t, name="command", text="None", font=font)
-    command.place(x=20, y=110)
+    command.place(x=20, y=160)
     timertick()
     t.mainloop()
 
@@ -59,8 +62,30 @@ def defects_label(a):
     t.children["defects"].configure(text=str("Defects = %s" % a))
 
 
+def defects_filtered_label(a):
+    t.children["defects_filtered"].configure(text=str("Defects filtered = %s" % a))
+
+
 def command_label(a):
     t.children["command"].configure(text=str("Command = %s" % a))
+
+
+def check_command(c):
+    if c == 1:
+        # play()
+        return "PLAY"
+    elif c == 2:
+        # pause()
+        return "PAUSE"
+    elif c == 3:
+        # move_next()
+        return "NEXT"
+    elif c == 4:
+        # move_prev()
+        return "PREVIOUS"
+    elif c == 5:
+        # vol_up()
+        return "VOLUME UP"
 
 
 if __name__ == '__main__':
@@ -72,97 +97,78 @@ if __name__ == '__main__':
         # 1. Constructing Region Of Interest (ROI)
         # read frame from camera
         _, img = cap.read()
+        # add rectangle for 'target scanning area' 300x300
+        cv2.rectangle(img, (350, 350), (50, 50), (0, 255, 0), 0)
+        # crop input image to 300x300
+        crop_img = img[50:350, 50:350]
         # convert image to gray scale
-        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        gray = cv2.cvtColor(crop_img, cv2.COLOR_BGR2GRAY)
         # use Gaussian blur
-        blur = cv2.GaussianBlur(gray, (5, 5), 0)
+        blur = cv2.GaussianBlur(gray, (35, 35), 0)
         # apply threshold to get black and white (binary) image (ROI)
-        _, thresh1 = cv2.threshold(blur, 70, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)
+        _, thresh1 = cv2.threshold(blur, 127, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)
+        if debug:
+            cv2.imshow('Threshold on cropped image', thresh1)
 
         # 2. Analyze ROI
         # find contours
         _, contours, hierarchy = cv2.findContours(thresh1, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+        # _, contours, hierarchy = cv2.findContours(thresh1.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
 
-        # empty matrix for output data to show later
-        drawing = np.zeros(img.shape, np.uint8)
+        # get max contour area
+        cnt = max(contours, key=lambda x: cv2.contourArea(x))
 
-        # get max area
-        max_area = 0
-        for i in range(len(contours)):
-            cnt = contours[i]
-            area = cv2.contourArea(cnt)
-            if area > max_area:
-                max_area = area
-                ci = i
-
-        try:
-            # get selected set of contours (biggest area)
-            cnt = contours[ci]
-        except Exception as e:
-            raise Camera.TooDarkError(e)
-
-        # find convex hulls using contours
+        # convex hull
+        x, y, w, h = cv2.boundingRect(cnt)
+        cv2.rectangle(crop_img, (x, y), (x + w, y + h), (0, 0, 255), 0)
         hull = cv2.convexHull(cnt)
-        moments = cv2.moments(cnt)
-        if moments['m00'] != 0:
-            cx = int(moments['m10'] / moments['m00'])  # cx = M10/M00
-            cy = int(moments['m01'] / moments['m00'])  # cy = M01/M00
 
-        # plot output contours to show
-        centr = (cx, cy)  # center point of ROI
-        cv2.circle(img, centr, 5, [0, 0, 255], 2)
-        cv2.drawContours(drawing, [cnt], 0, (0, 255, 0), 2)
-        cv2.drawContours(drawing, [hull], 0, (0, 0, 255), 2)
-
-        cnt = cv2.approxPolyDP(cnt, 0.01 * cv2.arcLength(cnt, True), True)
+        # contours
+        drawing = np.zeros(crop_img.shape, np.uint8)
+        cv2.drawContours(drawing, [cnt], 0, (0, 255, 0), 0)
+        cv2.drawContours(drawing, [hull], 0, (0, 0, 255), 0)
         hull = cv2.convexHull(cnt, returnPoints=False)
-        # print("hulls = %s" % hull.shape[0])
 
-        # compute defects using contours and hulls
+        # compute defects
         defects = cv2.convexityDefects(cnt, hull)
-        # print("defects = %s" % defects.shape[0])
-        mind = 0
-        maxd = 0
-
-        # plot convexity defects
+        count_defects = 0
+        cv2.drawContours(thresh1, contours, -1, (0, 255, 0), 3)
         for i in range(defects.shape[0]):
             s, e, f, d = defects[i, 0]
             start = tuple(cnt[s][0])
             end = tuple(cnt[e][0])
             far = tuple(cnt[f][0])
-            dist = cv2.pointPolygonTest(cnt, centr, True)
-            cv2.line(img, start, end, [0, 255, 0], 2)
-            cv2.circle(img, far, 5, [0, 0, 255], -1)
-        # print(defects.shape[0] - 1)
+            # dist
+            a = math.sqrt((end[0] - start[0]) ** 2 + (end[1] - start[1]) ** 2)
+            b = math.sqrt((far[0] - start[0]) ** 2 + (far[1] - start[1]) ** 2)
+            c = math.sqrt((end[0] - far[0]) ** 2 + (end[1] - far[1]) ** 2)
+            angle = math.acos((b ** 2 + c ** 2 - a ** 2) / (2 * b * c)) * 57
+            if angle <= 90:
+                count_defects += 1
+                cv2.circle(crop_img, far, 3, [255, 0, 0], -1)
+            # dist = cv2.pointPolygonTest(cnt,far,True)
+            cv2.line(crop_img, start, end, [0, 255, 0], 2)
+            if debug:
+                cv2.circle(crop_img, far, 5, [0, 0, 255], -1)
 
         # show analyzed input/output
-        cv2.imshow('output', drawing)
+        if debug:
+            all_img = np.hstack((drawing, crop_img))
+            cv2.imshow('Contours', all_img)
+
         cv2.imshow('input', img)
 
-        com = False
-        df = defects.shape[0]
-        if df == 6:
-            df = "VOLUME UP"
-            # vol_up()
-        elif df == 5:
-            com = "PLAY"
-            # play()
-        elif df == 4:
-            com = "PAUSE"
-            # pause()
-        elif df == 3:
-            com = "NEXT"
-            move_next()
-        elif df == 2:
-            com = "PREVIOUS"
-            # move_prev()
+        # check what command to execute and run it
+        com = check_command(count_defects)
 
-        k = cv2.waitKey(100)
+        k = cv2.waitKey(50)
         # got ESC key? if yes - exit!
         if k == 27:
             break
 
+        # submit some data to GUI
         submit_to_tkinter(hull_label, str(hull.shape[0]))
         submit_to_tkinter(defects_label, str(defects.shape[0]))
+        submit_to_tkinter(defects_filtered_label, str(count_defects))
         if com:
             submit_to_tkinter(command_label, com)
